fc9ad0f5ba303c6ce075d93f7e1c2a74fb1e8ee8
Ferenc Nagy <ferenc.nagy.jr@protonmail.com>
2026-03-08 17:13:22 -0400

    Add multisampling

--- src/main.c
+++ src/main.c
@@ -79,6 +79,25 @@ SDL_GPUShader *ShaderLoad(SDL_GPUDevice *device,
     return shader;
 }
 
+SDL_GPUSampleCount GetHighestSupportedSampleCount(SDL_GPUDevice *device, SDL_Window *window) {
+    SDL_GPUTextureFormat format = SDL_GetGPUSwapchainTextureFormat(device, window);
+    SDL_GPUSampleCount requestedSampleCounts[] = {
+        SDL_GPU_SAMPLECOUNT_8,
+        SDL_GPU_SAMPLECOUNT_4,
+        SDL_GPU_SAMPLECOUNT_2,
+        SDL_GPU_SAMPLECOUNT_1,
+    };
+
+    for (int i = 0; i < (sizeof(requestedSampleCounts) / sizeof(requestedSampleCounts[0])); ++i) {
+        if (SDL_GPUTextureSupportsSampleCount(device, format, requestedSampleCounts[i])) {
+            SDL_Log("Highest supported sample count (MSAA) = %dx", 2 << requestedSampleCounts[i]);
+            return requestedSampleCounts[i];
+        }
+    }
+
+    return 0;
+}
+
 int main(int argc, char **argv) {
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("Error initializing SDL: %s", SDL_GetError());
@@ -202,6 +221,13 @@ int main(int argc, char **argv) {
                 .fill_mode = SDL_GPU_FILLMODE_FILL,
                 .cull_mode = SDL_GPU_CULLMODE_BACK,
                 .front_face = SDL_GPU_FRONTFACE_COUNTER_CLOCKWISE,
+            },
+        .multisample_state =
+            {
+                .enable_alpha_to_coverage = false,
+                .enable_mask = false,
+                .sample_mask = 0,
+                .sample_count = GetHighestSupportedSampleCount(device, window),
             },
     };
 
